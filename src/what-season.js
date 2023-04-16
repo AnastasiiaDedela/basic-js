26d742d73e3eb7af039cd9a9176e83e0e8005379
Anastasiia Dedela <nastyadedela@gmail.com>
2023-04-16 11:21:32 +0200

    fix: fixed conditions

--- src/what-season.js
+++ src/what-season.js
@@ -24,9 +24,9 @@ function getSeason(date) {
   let month = date.getMonth()+1
   if(month == 1 || month == 2 || month == 12){
     return "winter"
-  } else if(month == 3 || month == 4 || month == 5){
+  } else if(month >= 3 && month <= 5){
     return "spring"
-  }else if(month == 6 || month == 7 || month == 8){
+  }else if(month >= 6 && month <= 8){
     return "summer"
   }else {
     return "autumn"
